2a976583c21917fc92dc743ce8883eba39046332
Maxim Meresenschi <maxim.meresenschi@amdaris.com>
2020-02-14 12:09:41 +0200

    jfjiorere

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Exceptions
 {
@@ -7,28 +8,67 @@ namespace Exceptions
         static void Main(string[] args)
         {
             House house = new House(1000, "Calea Iesilor str.");
-            Person person1 = new Person(null, "Max");
-            Person person2 = new Person(null, "Vlad");
+            Person person1 = new Person(null, "Vlad");
+            Person person2 = new Person(null, "Max");
             Console.WriteLine($"Person1 house before selling: { person1.House?.Address }");
             Console.WriteLine($"Person2 house before selling: { person2.House?.Address }");
+            #region Catch exception with if statement before it happens in the SellHouse method
+            //if (person1.House.Address == null)
+            //{
+            //    Console.WriteLine("Person1 doesn't have a house for selling");
+            //}
+            //else
+            //{
+            //    SellHouse(person1, person2);
+            //}
+            #endregion
+            StreamWriter streamWriter = new StreamWriter("a.txt"); // try inside try?
             try
             {
+                house.Price = 40000;
                 SellHouse(person1, person2);
+                Console.WriteLine($"Person1 house after selling: { person1.House?.Address }");
+                Console.WriteLine($"Person2 house after selling: { person2.House?.Address }");
+                House[] houses =
+                {
+                    new House(30000, "kkdkdkd"),
+                    new House(100000, "dewkoe"),
+                    new House(500000, "eirie")
+                };
+                int overallPrice = CalculateOverallPrice(houses);
+                streamWriter.Write(overallPrice);
             }
-            catch (NoHouseException e)
+            catch (NoHouseException e) when (person1.Name.Equals("Max")) // why use when???
             {
                 Console.WriteLine(e.Source);
                 Console.WriteLine(e.StackTrace);
                 Console.WriteLine(e.Message);
             }
-            catch (IndexOutOfRangeException)
+            catch (NoHouseException e)
             {
-
+                person1.House = house;
+                SellHouse(person1, person2);
             }
-            Console.WriteLine($"Person1 house after selling: { person1.House?.Address }");
-            Console.WriteLine($"Person2 house after selling: { person2.House?.Address }");
-        }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                streamWriter.Close();
+            }
 
+        }
+        static int CalculateOverallPrice(House[] houses)
+        {
+            int res = 0;
+            foreach (var house in houses)
+            {
+                res += house.Price;
+            }
+
+            return res;
+        }
         static void SellHouse(Person person1, Person person2)
         {
             if (person1.House == null)
@@ -40,16 +80,6 @@ namespace Exceptions
                 person2.House = person1.House;
                 person1.House = null;
             }
-
-            //if (person1.House == null)
-            //{
-            //    Console.WriteLine("Person1 doesn't have a house to sell");
-            //}
-            //else
-            //{
-            //    person2.House = person1.House;
-            //    person1.House = null;
-            //}
         }
     }
 
@@ -66,7 +96,25 @@ namespace Exceptions
 
     class House
     {
-        public int Price { get; set; }
+        private int price;
+        public int Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("value");
+                }
+                else
+                {
+                    price = value;
+                }
+            }
+        }
         public string Address { get; set; }
         public House(int price, string address)
         {
@@ -77,11 +125,10 @@ namespace Exceptions
     }
     class NoHouseException : Exception
     {
+        public string Message { get; set; }
         public NoHouseException(string name)
         {
-            Console.WriteLine($"{ name } doesn't have a house to sell");
+            Message = $"Exception: { name } has no house for selling";
         }
     }
-
-
 }
